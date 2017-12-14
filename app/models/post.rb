25c3e9f9836c49c8e0d77c8d5684b6500fe78aa9
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-14 21:06:01 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,3 @@ class Post < ActiveRecord::Base
   has_many :post_tags
   has_many :tags, through: :post_tags
 end
-
-
-ActiveRecord::HasManyThroughSourceAssociationNotFoundError: Could not find the source association(s) "tag" or :tags in model PostTag. Try 'has_many :t
-ags, :through => :post_tags, :source => <name>'. Is it one of ?
