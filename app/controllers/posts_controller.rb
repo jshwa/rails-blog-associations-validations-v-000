a606030e24eca391ad34125037e62b5553e78e03
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-14 22:24:46 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,6 +50,6 @@ class PostsController < ApplicationController
     end
 
     def post_params
-      params.require(:post).permit(:name, :content, :tags => [])
+      params.require(:post).permit(:name, :content, :tag_ids => [])
     end
 end
